8c4674581fc9cfd3c194dac7a9456e7f42eb96a5
SuhyeonBae <suhyeon.b01@gmail.com>
2020-12-04 16:08:47 +0900

    8. React Router & Link to Nav

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 import GlobalStyle from './components/GlobalStyle';
 // Import pages
 import AboutUs from './pages/AboutUs';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
 import Nav from './components/Nav';
+//Router
+import {Switch, Route} from 'react-router-dom';
 
 
 function App() {
@@ -11,7 +15,17 @@ function App() {
     <div className="App">
       <GlobalStyle />
       <Nav /> 
-      <AboutUs />
+      <Switch>
+        <Route path='/' exact>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
