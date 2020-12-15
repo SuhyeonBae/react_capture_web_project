786e058e90dfaa339af26027ae788e697d54f59c
SuhyeonBae <suhyeon.b01@gmail.com>
2020-12-15 14:59:11 +0900

    13. modify toggle effect

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -2,51 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import {About} from '../styles';
 import Toggle from './Toggle';
+import {AnimateSharedLayout} from 'framer-motion';
 
 const FaqSection = () => {
   return (
       <Faq>
         <h2>Any questions? <span>FAQ</span></h2>
-        <Toggle>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                  <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum non, ea, animi quod nobis voluptatibus laudantium est laborum enim a ut perferendis tempore? Sequi quos, eaque itaque tempore consequatur unde.</p>
-                </div>
-            </div>
-            <div className="faq__line"></div>
-        </Toggle>
-        <Toggle>
-          <div className="question">
-              <h4>Daily Schedule</h4>
+        <AnimateSharedLayout>
+          <Toggle title='How Do I Start?'>
+              <div className="question">
+                  <div className="answer">
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum non, ea, animi quod nobis voluptatibus laudantium est laborum enim a ut perferendis tempore? Sequi quos, eaque itaque tempore consequatur unde.</p>
+                  </div>
+              </div>
+          </Toggle>
+
+          <Toggle title='Daily Schedule'>
               <div className="answer">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia, amet!</p>
               </div>
-          </div>
-          <div className="faq__line"></div>
-        </Toggle>
-        <Toggle>
-          <div className="question">
-              <h4>Different Payment Methods</h4>
+          </Toggle>
+
+          <Toggle title='Different Payment Methods'>
               <div className="answer">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum voluptate eligendi vitae reprehenderit assumenda illo? Ea natus recusandae iure saepe.</p>
               </div>
-          </div>
-          <div className="faq__line"></div>
-        </Toggle>
-        <Toggle>
-          <div className="question">
-              <h4>What Products do you offer.</h4>
+          </Toggle>
+
+          <Toggle title='What Products do you offer.'>
               <div className="answer">
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing.</p>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit, ducimus! Consequuntur illo doloremque quo temporibus quam natus sequi accusamus fugiat mollitia itaque. Harum, laboriosam iure.</p>
               </div>
-              <div className="faq__line"></div>
-          </div>
-        </Toggle>
+          </Toggle>          
+        </AnimateSharedLayout>
       </Faq>
   )
 };
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
+import {motion} from 'framer-motion';
 
-const Toggle = ({children}) => { //children extraction
-  const [toggle, setToggle] = useState(true);
+const Toggle = ({children, title}) => { //children extraction
+  const [toggle, setToggle] = useState(false);
   return(
-    <div onClick={() => setToggle(!toggle)}>
+    <motion.div layout className='question' onClick={() => setToggle(!toggle)}>
+      <motion.h4 layout> {title} </motion.h4>
       {toggle ? children : ''}
-    </div>
+      <div className="faq__line"></div>
+    </motion.div>
   );
 };
 
